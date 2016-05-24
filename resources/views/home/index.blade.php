fdd3c53f6b8f1bcc64d02dc57d565eee57668dd5
moronghui <1570987718@qq.com>
2016-05-25 00:59:57 +0800

    主页
    主页页面的实现

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -11,10 +11,10 @@
 <body>
 <div class="comWidth header">
 	<div class="info">
-		<img src="" alt="头像">
+		<img src="{{ asset('/uploads/default.jpg')}}" alt="头像">
 		<p><a href="#">倾听的博客</a></p>
 	</div>
-	<div class="nav">
+	<div class="nav right" >
 		<ul>
 			<li><a href="#">首页</a></li>
 			<li><a href="#">博文目录</a></li>
@@ -25,18 +25,63 @@
 </div><!-- header结束 -->
 
 <div class="content comWidth">
-	<div class="c_left">
-		<div class="title">
-			<span>个人资料</span>
-			<span><a href="#">[管理]</a></span>
+	<div class="c_left left">
+		<!-- 个人资料 -->
+		<div class="data">
+			<div class="title">
+				<span class="left">个人资料</span>
+				<span class="right"><a href="#">[管理]</a></span>
+			</div>
+			<div class="img">
+				<img src="{{ asset('/uploads/default.jpg')}}" alt="头像">
+			</div>
+			<div class="name">倾听</div>
 		</div>
-		<div class="img">
-			<img src="" alt="头像">
+		<!-- 评论 -->
+		<div class="comment">
+			<div class="title">
+				<span class="left">评论</span>
+				<span class="right"><a href="#">[管理]</a></span>
+			</div>
+			<div class="item">
+				<div class="user">
+					<span class="left">新浪官博</span> 
+					<span class="right">05-01 14:45</span>
+				</div>
+				<div class="c_content">
+					<span class="left">恭喜你已开通...</span>
+					<span class="right"><a href="#">[删除]</a></span> 
+				</div>				
+			</div>
+			<div class="item">
+				<div class="user">
+					<span class="left">新浪官博</span> 
+					<span class="right">05-01 14:45</span>
+				</div>
+				<div class="c_content">
+					<span class="left">恭喜你已开通...</span>
+					<span class="right"><a href="#">[删除]</a></span> 
+				</div>				
+			</div>
+			<div class="more">
+					<a href="#" class="right">更多>></a>
+			</div>
 		</div>
-		<div class="name">倾听</div>
-
+		<!-- 分类 -->
+		<div class="category">
+			<div class="title">
+				<span class="left">评论</span>
+				<span class="right"><a href="#">[管理]</a></span>
+			</div>
+			<ul>
+				<li><a href="#">全部博文</a>(11)</li>
+				<li><a href="#">PHP</a>(3)</li>
+				<li><a href="#">android</a>(4)</li>
+				<li><a href="#">laravel</a>(4)</li>
+			</ul>
+		</div>
 	</div>
-	<div class="c_right">
+	<div class="c_right right">
 		<div class="head">
 			<span>博文</span>
 			<span><a href="#">[管理]</a></span>
