066f78da4b149b7f2d9b234fd41bdfbb6c5e58f2
moronghui <1570987718@qq.com>
2016-05-24 19:01:59 +0800

    数据库
    建立数据表模型，进行数据迁移

--- database/migrations/2016_05_24_094530_create_admins_table.php
+++ database/migrations/2016_05_24_094530_create_admins_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration {
+class CreateAdminsTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,16 +12,12 @@ class CreateUsersTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('users', function(Blueprint $table)
+		Schema::create('admins', function(Blueprint $table)
 		{
 			$table->increments('id');
 			$table->string('name');
 			$table->string('email')->unique();
-			$table->string('password', 60);
-			$table->string('face');
-			$table->string('word');
-			$table->string('sex')->default('男');
-			$table->rememberToken();
+			$table->string('password',60);
 			$table->timestamps();
 		});
 	}
@@ -33,7 +29,7 @@ class CreateUsersTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('users');
+		Schema::drop('admins');
 	}
 
 }
---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="zh-CN">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="博客">
+<link rel="stylesheet" type="text/css" href="{{ asset('/home/css/main.css')}}">
+<link rel="stylesheet" type="text/css" href="{{ asset('/home/css/reset.css')}}">
+<title>博客</title>
+
+<body>
+<div class="comWidth header">
+	<div class="info">
+		<img src="" alt="头像">
+		<p><a href="#">倾听的博客</a></p>
+	</div>
+	<div class="nav">
+		<ul>
+			<li><a href="#">首页</a></li>
+			<li><a href="#">博文目录</a></li>
+			<li><a href="#">发博文</a></li>
+			<li><a href="#">个人中心</a></li>
+		</ul>
+	</div>
+</div><!-- header结束 -->
+
+<div class="content comWidth">
+	<div class="c_left">
+		<div class="title">
+			<span>个人资料</span>
+			<span><a href="#">[管理]</a></span>
+		</div>
+		<div class="img">
+			<img src="" alt="头像">
+		</div>
+		<div class="name">倾听</div>
+
+	</div>
+	<div class="c_right">
+		<div class="head">
+			<span>博文</span>
+			<span><a href="#">[管理]</a></span>
+		</div>
+		<div class="item">
+			<h2>数据库乱码<span>(2016-05-17 17:41)</span><span>[管理]</span><span>[删除]</span></h2>
+			<h3>标签：<span>mysql</span> &nbsp;&nbsp;分类：<span>php</span></h3>
+			<article>
+				正文部分正文部分正文部分正文部分正文部分正文部分
+				正文部分正文部分正文部分正文部分
+			</article>
+			<div class="flooter">
+				<a href="#">评论</a><span>(3)</span>&nbsp;<a href="#">查看正文</a>
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
